35b426e0475087dd57a715250a2b3afaa1397208
EdgarKoterle <EdgarKoterle@akademiasovy.sk>
2018-01-11 12:36:40 +0100

    Added triangle

--- src/sk/akademiasovy/geometry/Rectangle.java
+++ src/sk/akademiasovy/geometry/Rectangle.java
@@ -2,41 +2,58 @@ package sk.akademiasovy.geometry;
 
 public class Rectangle {
 
-        private double a;
-        private double b;
+    private double a;
+    private double b;
 
-        public rectangle(double a)
-        {
-            this.a=a;
-            this.a=b;
-        }
+    public double getA() {
+        return a;
+    }
 
-        public rectangle(double a, double b)
-        {
-            this.a=a;
-            this.b=b;
-        }
+    public void setA(double a) {
+        this.a = a;
+    }
 
-        public rectangle()
-        {
-        }
+    public double getB() {
+        return b;
+    }
 
-        public double getArea()
-        {
-            return a*b;
-        }
-        public double getPerimeter()
-        {
-            return 2*a + 2*b;
-        }
-        public double getDiagonal()
-        {
-            return  Math.sqrt(a*a+b*b);
+    public void setB(double b) {
+        this.b = b;
+    }
 
-        }
-        public void transpuse()
-        {
-            double temp;
-            temp=a;
-        }
+    public rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public rectangle(double value){
+        this.a=value;
+        this.b=value;
+    }
+
+    public rectangle(){
+        a=0;
+        b=0;
+    }
+
+
+    public double getArea(){
+        return a*b;
+    }
+
+    public double getPerimeter(){
+        return 2*(a+b);
+    }
+
+    public double getDiagonal(){
+        double diagonal=Math.sqrt(a*a+b*b);
+        return diagonal;
+    }
+
+    public void transpose(){
+        double temp;
+        temp=a;
+        a=b;
+        b=temp;
+    }
 }
--- src/sk/akademiasovy/main/Main.java
+++ src/sk/akademiasovy/main/Main.java
@@ -1,6 +1,8 @@
 package sk.akademiasovy.main;
 
 import sk.akademiasovy.geometry.Square;
+import sk.akademiasovy.geometry.Rectangle;
+import sk.akademiasovy.geometry.Triangle;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,5 +12,19 @@ public class Main {
         System.out.println("Area of square1 is "+square1.getArea());
         System.out.println("Perimeter of square1 is "+square1.getPerimeter());
         System.out.println("Diagonal of square1 is "+square1.getDiagonal());
+
+        Rectangle rectangle1=new Rectangle(8.5, 6.9);
+        Rectangle rectangle2=new Rectangle();
+
+        System.out.println("Area of rectangle1 is "+rectangle1.getArea());
+        System.out.println("Perimeter of rectangle1 is "+rectangle1.getPerimeter());
+        System.out.println("Diagonal of rectangle1 is "+rectangle1.getDiagonal());
+
+        Triangle triangle1=new Triangle (3, 4, 5);
+        Triangle triangle2=new Triangle ();
+
+        System.out.println("Area of triangle1 is "+triangle1.getArea());
+        System.out.println("Perimeter of triangle1 is "+triangle1.getPerimeter());
+        System.out.println("Diagonal of triangle1 is "+triangle1.getDiagonal());
     }
 }
